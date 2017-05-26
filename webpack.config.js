d2f4523891fcbd086197be30667dcf61b5793a83
Justin Thomas <justin.stephen.thomas@gmail.com>
2017-05-26 10:40:07 -0400

    update webpack config to use babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 var path = require('path');
 
-module.exports = {
+const webpackConfig = {
 	entry: './app/index.js',
 	output: {
 		filename: 'app.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader'
+			}
+		]
 	}
 };
+
+module.exports = config;
