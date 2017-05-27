0f7858e2b10ed83b5cf2dad6c57dd2358f1538cf
Justin Thomas <justin.stephen.thomas@gmail.com>
2017-05-27 15:07:36 -0400

    add eslintrc and editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
-var path = require('path');
+const path = require('path');
 
 const webpackConfig = {
-	entry: './app/index.js',
-	output: {
-		filename: 'app.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: 'babel-loader'
-			}
-		]
-	}
+    entry: './app/index.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader'
+        }]
+    }
 };
 
 module.exports = config;
